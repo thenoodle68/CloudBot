868ebccb02ae09a7561c7397b2760ad44e60bf5d
Fletcher Boyd <me@fletcherjamesboyd.com>
2013-09-07 20:40:48 +0800

    Half fixed.  Now start style tags aren't working.

--- plugins/util/formatting.py
+++ plugins/util/formatting.py
@@ -1,4 +1,5 @@
 import re
+import pprint
 
 
 def raw(format_string):
@@ -7,7 +8,7 @@ def raw(format_string):
                      'black': '\x031',
                      'dblue': '\x032',
                      'dgreen': '\x033',
-                     'dred': '\x034',
+                     'red': '\x034',
                      'brown': '\x035',
                      'purple': '\x036',
                      'gold': '\x037',
@@ -28,6 +29,7 @@ def raw(format_string):
     tags = re.finditer(r"\[/?\w+\]", format_string)  # Finds all instances of [x] and [/x] that are one word.
     for x in tags:
         curstate = dict(states[-1])
+        curstate['cancel'] = []
         text = x.group()[1:-1]  # Take out the brackets
         end = 0
         curstate['pos'] = x.span()
@@ -39,7 +41,7 @@ def raw(format_string):
         elif text in stuff['col'] and end == 1:  # Closing colour tag
             for st in states[::-1]:  # Find what colour was set before
                 if st['col'] != text:
-                    curstate['col'] = text
+                    curstate['col'] = st['col']
         elif end == 0:
             curstate['add'].append(text)  # Add the tag to the list of active tags
             if text in curstate['cancel']:  # Remove it from the cancel list
@@ -49,7 +51,7 @@ def raw(format_string):
             if text in curstate['add']:
                 curstate['add'].remove(text)
         states.append(curstate)  # Add this state to the list of states to be processed.
-
+    print pprint.pprint(states)
     for x in states[::-1]:  # Apply formatting from the end
         state = ""
         if x['col']:  # Does the current state require colour formatting?
@@ -61,9 +63,13 @@ def raw(format_string):
         for y in x['cancel']:
             state += stuff['style'][y]
         format_string = format_string[:x['pos'][0]] + state + format_string[x['pos'][1]:]  # Should probably regex.
+        #format_string = re.sub(r"\[/?\w+\]", state, format_string)
     return format_string
 
 
 def err(format_string):
     """Format the string with standard error styling"""
     return "\x034\x02{}\x0f".format(format_string)
+
+if __name__ == "__main__":
+    print raw("This text is [dgreen]green[/dgreen] while this [red]red[/red] word is [b]bold[/b] butts blah blah blah")
